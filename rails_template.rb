a985152825549658aac6097624f7ee044710dfe1
Tony Coconate <me@tonycoconate.com>
2012-01-12 22:48:41 -0600

    fixes

--- rails_template.rb
+++ rails_template.rb
@@ -195,18 +195,16 @@ inject_into_file 'app/models/user.rb', :before => 'end' do <<-RUBY
   mount_uploader :avatar, AvatarUploader
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-    data = access_token.extra.raw_info
-
-    if user = User.where(:email => data.email).first
+    if user = User.where(:email => access_token.info.email).first
       user
-    else # Create a user with a stub password.
-      User.create!(:email => data.email, :password => Devise.friendly_token[0, 20], :remote_avatar_url => data.image, :data => data)
+    else
+      User.create!(:email => access_token.info.email, :password => Devise.friendly_token[0, 20], :remote_avatar_url => access_token.info.image, :data => access_token.to_hash)
     end
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['user_hash']
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
         user.email = data['email']
       end
     end
