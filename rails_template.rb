88c590401cc9a8834781759f427e07c0846e2c24
Tony Coconate <me@tonycoconate.com>
2012-03-20 13:09:32 -0500

    fixes in css and devise permissions

--- rails_template.rb
+++ rails_template.rb
@@ -195,11 +195,7 @@ RUBY
 end
 
 inject_into_file 'config/initializers/devise.rb', :after => "Devise.setup do |config|\n" do <<-RUBY
-  user_permissions     = %w(user_about_me user_activities user_birthday user_checkins user_education_history user_events user_groups user_hometown user_interests user_likes user_location user_notes user_online_presence user_photo_video_tags user_photos user_questions user_relationships user_relationship_details user_religion_politics user_status user_videos user_website user_work_history email)
-  friends_permissions  = %w(friends_about_me friends_activities friends_birthday friends_checkins friends_education_history friends_events friends_groups friends_hometown friends_interests friends_likes friends_location friends_notes friends_online_presence friends_photo_video_tags friends_photos friends_questions friends_relationships friends_relationship_details friends_religion_politics friends_status friends_videos friends_website friends_work_history)
-  extended_permissions = %w(read_friendlists read_insights read_mailbox read_requests read_stream xmpp_login ads_management create_event manage_friendlists manage_notifications offline_access publish_checkins publish_stream rsvp_event sms publish_actions)
-
-  config.omniauth :facebook, Settings.facebook.app_id, Settings.facebook.app_secret, :scope => (user_permissions + friends_permissions + extended_permissions).join(',')
+  config.omniauth :facebook, Settings.facebook.app_id, Settings.facebook.app_secret, :scope => 'email,publish_actions'
 RUBY
 end
 
